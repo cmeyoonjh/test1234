42a1ef10d9f14cc6689cda83c7b3bfa576fb8f50
cmeyoonjh <cmeyoonjh>
2018-07-10 15:13:30 +0900

    rollback4 commit

--- truffle.js
+++ truffle.js
@@ -12,3 +12,13 @@ module.exports = {
     }
   }
 }
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id
+      from:'0xe6e0a47a0af55919e95b42f232156e0f6fe1410e',
+      gas:4712901
+    }
+  }
+}
